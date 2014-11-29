570f398d33617107070c5ec75858662585e1e78f
AkuseruSan <AkuseruAkai@gmail.com>
2014-11-29 14:13:09 +0100

    GUISystem Started

--- Assets/Scripts/World/GUISystem.cs
+++ Assets/Scripts/World/GUISystem.cs
@@ -3,18 +3,40 @@ using System.Collections;
 
 public class GUISystem : MonoBehaviour 
 {
+    private enum MenuState {Main, Selector, OAudio, OVideo, OKeys, OAdvanced }
 
+    private MenuState menuState = MenuState.Main;
 
+    public GUIStyle menuStyle;
 
-	void Start () 
+    public Texture background;
+
+    void OnGUI()
     {
-	    
-	}
-	
+        if (true)
+        {
+            GUI.Box(new Rect(20, 20, Screen.width - 40, Screen.height - 20), GUIContent.none, menuStyle);
 
-
-	void Update () 
-    {
-	
-	}
+            switch (menuState)
+            {
+                case MenuState.Main:
+                    {
+                        if (GUI.Button(new Rect(Screen.width / 2 - 150, Screen.height / 2 - 60, 220, 80), "New Game", menuStyle)) { }
+                    }
+                    break;
+                case MenuState.Selector:
+                    break;
+                case MenuState.OAudio:
+                    break;
+                case MenuState.OVideo:
+                    break;
+                case MenuState.OKeys:
+                    break;
+                case MenuState.OAdvanced:
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
 }
