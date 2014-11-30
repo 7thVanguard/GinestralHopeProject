abdbec8ca5d59889d613f88837feeb373c6aa2e6
Palaui <sucdepressec@gmail.com>
2014-11-30 19:07:52 +0100

    Little enemy AI
    Acting like a rotating turret

--- Assets/Scripts/World/HUD.cs
+++ Assets/Scripts/World/HUD.cs
@@ -113,6 +113,21 @@ public class HUD : MonoBehaviour
                             (new Rect(Screen.width * 40 / 100, Screen.height * 42 / 50, lateralButtonSize, lateralButtonSize),
                             developerAtlas,
                             new Rect(0 / 8f, 1 / 8f, 1 / 8f, 1 / 8f));
+                        GUI.Label(new Rect(Screen.width * 41 / 100, Screen.height * 42 / 50, lateralButtonSize, lateralButtonSize), "1");
+
+                        // Wooden Bridge
+                        GUI.DrawTextureWithTexCoords
+                            (new Rect(Screen.width * 60 / 100, Screen.height * 42 / 50, lateralButtonSize, lateralButtonSize),
+                            developerAtlas,
+                            new Rect(0 / 8f, 3 / 8f, 1 / 8f, 1 / 8f));
+                        GUI.Label(new Rect(Screen.width * 61 / 100, Screen.height * 42 / 50, lateralButtonSize, lateralButtonSize), "4");
+
+                        // Bomb
+                        GUI.DrawTextureWithTexCoords
+                            (new Rect(Screen.width * 70 / 100, Screen.height * 42 / 50, lateralButtonSize, lateralButtonSize),
+                            developerAtlas,
+                            new Rect(2 / 8f, 3 / 8f, 1 / 8f, 1 / 8f));
+                        GUI.Label(new Rect(Screen.width * 71 / 100, Screen.height * 42 / 50, lateralButtonSize, lateralButtonSize), "5");
 
 
                         // Inventory
