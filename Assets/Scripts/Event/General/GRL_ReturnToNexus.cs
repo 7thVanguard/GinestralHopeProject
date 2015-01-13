883120c2e90f7526ecfe32cd77c540e816dbf053
Palaui <sucdepressec@gmail.com>
2015-01-14 00:23:40 +0100

    Return to nexus Event

---
+++ Assets/Scripts/Event/General/GRL_ReturnToNexus.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GRL_ReturnToNexus : MonoBehaviour 
+{
+    World world;
+
+
+
+    void Start()
+    {
+        BoxCollider boxCollider;
+        boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.size = new Vector3(4, 4, 4);
+        boxCollider.isTrigger = true;
+        this.world = gameObject.GetComponent<EventComponent>().world;
+        Transform.Destroy(gameObject.GetComponent<EventComponent>());
+    }
+
+
+    void OnTriggerStay(Collider other)
+    {
+        if (GameFlow.gameMode == GameFlow.GameMode.PLAYER)
+            if (other.tag == "Player")
+            {
+                
+            }
+    }
+}
