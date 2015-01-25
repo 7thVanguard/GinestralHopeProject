5b7bd9d2af3d1ae302d5be6b87b0d784c1faf8ce
Palaui <sucdepressec@gmail.com>
2015-01-25 02:20:21 +0100

    FirstCaverninaLevel events and level Update

---
+++ Assets/Scripts/Event/CaverninaFirstLevel/CFL_ThirdSignalPlatform.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CFL_ThirdSignalPlatform : MonoBehaviour 
+{
+    GameObject platform;
+    GameObject lever;
+
+
+    void Start()
+    {
+        RaycastHit impact;
+
+        if (Physics.Raycast(new Vector3(11f, 14, 22), new Vector3(0, 0, -1), out impact, 5))
+            lever = impact.transform.parent.gameObject;
+
+        // Spawn
+        platform = (GameObject)Instantiate(Resources.Load("Props/Geometry/SignalPlatform/SignalPlatform")) as GameObject;
+
+        platform.transform.position = new Vector3(13.5f, 14.75f, 19.5f);
+        platform.transform.eulerAngles = Vector3.zero;
+        platform.transform.localScale = new Vector3(4f, 4f, 0.49f);
+        platform.transform.parent = Global.world.creationsController.transform;
+
+        platform.GetComponent<SignalPlatformBehaviour>().initialPosition = platform.transform.position;
+        platform.GetComponent<SignalPlatformBehaviour>().endPosition = new Vector3(13.5f, 14.75f, 13f);
+    }
+
+
+    void Update()
+    {
+        if (lever.GetComponent<LeverBehaviour>().emiting)
+            platform.transform.GetComponent<SignalPlatformBehaviour>().onSignal = true;
+        else
+            platform.transform.GetComponent<SignalPlatformBehaviour>().onSignal = false;
+    }
+}
--- Assets/Scripts/Geometry/Behaviours/LeverBehaviour.cs
+++ Assets/Scripts/Geometry/Behaviours/LeverBehaviour.cs
@@ -6,12 +6,34 @@ public class LeverBehaviour : MonoBehaviour
     public bool locked = false;
     public bool emiting = false;
 
+    private bool keepPressed = false;
+
 
     void OnTriggerStay(Collider other)
     {
         if (GameFlow.gameMode == GameFlow.GameMode.PLAYER)
             if (other.tag == "Player")
-                if (Input.GetKeyUp(KeyCode.E))
-                    emiting = !emiting;
+            {
+                if (Input.GetKey(KeyCode.E))
+                {
+                    if (!keepPressed)
+                    {
+                        emiting = !emiting;
+                        keepPressed = true;
+                    }
+                }
+                else if (Input.GetKeyUp(KeyCode.E))
+                    keepPressed = false;
+                else if (Input.GetKeyDown(KeyCode.E))
+                    keepPressed = false;
+            }
+    }
+
+
+    void OnTriggerExit(Collider other)
+    {
+        if (GameFlow.gameMode == GameFlow.GameMode.PLAYER)
+            if (other.tag == "Player")
+                keepPressed = false;
     }
 }
