5bc1a2e138d261678de2f853cc595373a4a0c618
Palaui <sucdepressec@gmail.com>
2015-02-03 18:43:48 +0100

    Cavernina Boss half done

---
+++ Assets/Scripts/Event/CaverninaBoss/CB_FirstContactPlatform.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CB_FirstContactPlatform : MonoBehaviour 
+{
+    void Start()
+    {
+        GameObject platform = (GameObject)Instantiate(Resources.Load("Props/Geometry/Platform Contact/Platform Contact")) as GameObject;
+
+        platform.transform.position = new Vector3(37f, 4f, 32f);
+        platform.transform.eulerAngles = Vector3.zero;
+        platform.transform.localScale = new Vector3(3, 0.5f, 3);
+        platform.transform.parent = Global.world.creationsController.transform;
+
+        platform.GetComponent<ContactPlatformBehaviour>().initialPosition = platform.transform.position;
+        platform.GetComponent<ContactPlatformBehaviour>().endPosition = new Vector3(37f, 8f, 32f);
+    }
+}
---
+++ Assets/Scripts/Event/CaverninaBoss/CB_SecondConstantPlatform.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CB_SecondConstantPlatform : MonoBehaviour 
+{
+    void Start()
+    {
+        GameObject platform = (GameObject)Instantiate(Resources.Load("Props/Geometry/Platform Constant/Platform Constant")) as GameObject;
+
+        platform.transform.position = new Vector3(7f, 6.25f, 2.25f);
+        platform.transform.eulerAngles = Vector3.zero;
+        platform.transform.localScale = new Vector3(3, 0.5f, 3);
+        platform.transform.parent = Global.world.creationsController.transform;
+
+        platform.GetComponent<ConstantPlatformBehaviour>().initialPosition = platform.transform.position;
+        platform.GetComponent<ConstantPlatformBehaviour>().endPosition = new Vector3(17f, 6.25f, 2.25f);
+    }
+}
