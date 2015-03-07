a2635b8b1a17ba97f2a691b03a48ecd321e2dcca
Palaui <sucdepressec@gmail.com>
2015-03-07 13:35:31 +0100

    Update

---
+++ Assets/Scripts/Event/General/GRL_DoorDouble.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GRL_DoorDouble : MonoBehaviour 
+{
+    World world;
+
+    public GameObject firstDoor;
+    public GameObject secondDoor;
+    public Vector3 firstDoorObjectivePosition;
+    public Vector3 secondDoorObjectivePosition;
+
+    public Color ambient;
+
+    public bool activeOnEnter;
+
+    private float timeCounter = 0;
+    private bool active;
+
+
+    void Start()
+    {
+        this.world = gameObject.GetComponent<EventComponent>().world;
+        Transform.Destroy(gameObject.GetComponent<EventComponent>());
+    }
+
+
+    void Update()
+    {
+        if (active)
+        {
+            EventsLib.UpdateRenderambient();
+            EventsLib.UpdateDoorOpenDoubleSlider();
+
+            timeCounter -= Time.deltaTime;
+            if (timeCounter < 0)
+                active = false;
+        }
+    }
+
+
+    void OnTriggerStay(Collider other)
+    {
+        if (GameFlow.gameMode == GameFlow.GameMode.PLAYER)
+            if (other.tag == "Player")
+            {
+                if (activeOnEnter)
+                {
+                    EventsLib.SetRenderambient(ambient);
+                    EventsLib.SetDoorOpenDoubleSlider(firstDoor, firstDoorObjectivePosition, secondDoor, secondDoorObjectivePosition);
+
+                    timeCounter = 10;
+                    active = true;
+                }
+                else
+                {
+                    if (Input.GetKeyUp(KeyCode.E))
+                    {
+                        EventsLib.SetRenderambient(ambient);
+                        EventsLib.SetDoorOpenDoubleSlider(firstDoor, firstDoorObjectivePosition, secondDoor, secondDoorObjectivePosition);
+
+                        timeCounter = 10;
+                        active = true;
+                    }
+                }
+            }
+    }
+}
