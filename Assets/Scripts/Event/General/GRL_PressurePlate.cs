c8635c6a271e54574d63ea82374e3c63ac9b12ba
Palaui <sucdepressec@gmail.com>
2015-03-08 14:25:42 +0100

    Signal general events and 3th instance props

---
+++ Assets/Scripts/Event/General/GRL_PressurePlate.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GRL_PressurePlate : MonoBehaviour 
+{
+    private const float nonPressedHeight = -0.05f;
+    private const float pressedHeight = -0.15f;
+
+    public bool emitting;
+
+
+    void Update()
+    {
+        if (emitting)
+            transform.parent.localPosition = new Vector3(0, Mathf.Lerp(transform.parent.localPosition.y, pressedHeight, 0.1f), 0);
+        else
+            transform.parent.localPosition = new Vector3(0, Mathf.Lerp(transform.parent.localPosition.y, nonPressedHeight, 0.1f), 0);
+    }
+
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (other.transform.GetComponent<CharacterController>().isGrounded)
+            {
+                other.transform.parent = transform.parent;
+                emitting = true;
+            }
+            else
+            {
+                other.transform.parent = null;
+                emitting = false;
+            }
+        }
+    }
+
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.transform.parent = null;
+            emitting = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Event/General/GRL_SignalPlatform.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GRL_SignalPlatform : MonoBehaviour 
+{
+    public Vector3 activePosition;
+    public Vector3 nonActivePosition;
+
+    public GameObject emitter;
+
+    private bool emitting;
+
+
+    void Update()
+    {
+        if (emitter.GetComponent<GRL_PressurePlate>() != null)
+            emitting = emitter.GetComponent<GRL_PressurePlate>().emitting;
+
+        if (emitting)
+            transform.parent.position = Vector3.Lerp(transform.parent.position, activePosition, 0.05f);
+        else
+            transform.parent.position = Vector3.Lerp(transform.parent.position, nonActivePosition, 0.05f);
+    }
+
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.transform.parent = transform.parent;
+        }
+    }
+
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.transform.parent = null;
+        }
+    }
+}
