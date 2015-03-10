87b632d044fd8cba352fd73a527e606eb8018fb2
Palaui <sucdepressec@gmail.com>
2015-03-10 20:51:28 +0100

    Third Instance events finished

--- Assets/Scripts/Event/NewGameGH/NGGH_ThirdInstanceWagon.cs
+++ Assets/Scripts/Event/NewGameGH/NGGH_ThirdInstanceWagon.cs
@@ -4,7 +4,13 @@ using System.Collections.Generic;
 
 public class NGGH_ThirdInstanceWagon : MonoBehaviour 
 {
-	public Animation animator;
+    private Animation animator;
+    private float counter = 0;
+    private float timeToCount = 3;
+
+    private bool animationDone = false;     
+    private bool ready;                     // When ready to pick up the light
+	
 
 	void Start() 
 	{
@@ -13,12 +19,40 @@ public class NGGH_ThirdInstanceWagon : MonoBehaviour
 	}
 
 
+    void Update()
+    {
+        // Count three seconds from pushing the minecrat before we can pick up the light
+        if (animationDone && !ready)
+        {
+            counter += Time.deltaTime;
+
+            if (counter >= timeToCount)
+                ready = true;
+        }
+    }
+
+
     void OnTriggerStay(Collider other)
     {
         if (other.tag == "Player")
         {
             if (Input.GetKey(KeyCode.E))
-				transform.parent.animation.Play();
+            {
+                // Push the MineCart
+                if (!animationDone)
+                {
+                    transform.parent.animation.Play();
+                    animationDone = true;
+                }
+                // Player pick up light
+                else if (ready)
+                {
+                    GameObject lamp = transform.parent.FindChild("Lamp").gameObject;
+                    lamp.transform.parent = Global.player.playerObj.transform.FindChild("Mesh");
+                    lamp.transform.localPosition = new Vector3(0.3f, 0.8f, 0);
+                    Destroy(transform.GetComponent<NGGH_ThirdInstanceWagon>());
+                }
+            }
         }
     }
 }
