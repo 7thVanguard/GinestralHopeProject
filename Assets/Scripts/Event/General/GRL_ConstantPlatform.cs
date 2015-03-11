031bded0b31e84ba3c64c598642f06ea0fe6d1f5
Palaui <sucdepressec@gmail.com>
2015-03-11 23:18:32 +0100

    Platform logic on extyernal general events

---
+++ Assets/Scripts/Event/General/GRL_ConstantPlatform.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GRL_ConstantPlatform : MonoBehaviour
+{
+    public Vector3 initialPosition;
+    public Vector3 endPosition;
+    public float duration;
+
+    private float interpolation = 0;
+    private bool toEndPos = false;
+
+
+    void Sttart()
+    {
+        if (duration < 1)
+            duration = 1;
+    }
+
+
+    void Update()
+    {
+        if (toEndPos)
+            interpolation += Time.deltaTime / duration;
+        else
+            interpolation -= Time.deltaTime / duration;
+
+        if (interpolation > 0.99f)
+            toEndPos = false;
+        else if (interpolation < 0.01f)
+            toEndPos = true;
+
+        interpolation = Mathf.Clamp(interpolation, 0, 1);
+        transform.position = Vector3.Slerp(initialPosition, endPosition, interpolation);
+    }
+
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+            Global.player.playerObj.transform.parent = transform;
+    }
+
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+            Global.player.playerObj.transform.parent = null;
+    }
+}
---
+++ Assets/Scripts/Event/General/GRL_ContactPlatform.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GRL_ContactPlatform : MonoBehaviour 
+{
+    public Vector3 initialPosition;
+    public Vector3 endPosition;
+    public float duration;
+
+    private float interpolation = 0;
+    private bool onStay = false;
+
+
+    void Sttart()
+    {
+        if (duration < 1)
+            duration = 1;
+    }
+
+
+    void Update()
+    {
+        if (onStay)
+            interpolation += Time.deltaTime / duration;
+        else
+            interpolation -= Time.deltaTime / duration;
+
+        interpolation = Mathf.Clamp(interpolation, 0, 1);
+        transform.position = Vector3.Slerp(initialPosition, endPosition, interpolation);
+    }
+
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            onStay = true;
+            Global.player.playerObj.transform.parent = transform;
+        }
+    }
+
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            onStay = false;
+            Global.player.playerObj.transform.parent = null;
+        }
+    }
+}
