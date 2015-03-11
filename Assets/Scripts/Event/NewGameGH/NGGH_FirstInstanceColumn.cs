16f7fa7ec03150d038d825f1ee009142a721781a
Palaui <sucdepressec@gmail.com>
2015-03-11 18:06:45 +0100

    Broken column animation working

---
+++ Assets/Scripts/Event/NewGameGH/NGGH_FirstInstanceColumn.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NGGH_FirstInstanceColumn : MonoBehaviour 
+{
+    public GameObject column;
+    private bool finished = false;
+
+
+    void OnTriggerStay(Collider other)
+    {
+        if (!finished)
+            if (other.tag == "Player")
+            {
+                column.animation.Play();
+                finished = true;
+            }
+    }
+}
