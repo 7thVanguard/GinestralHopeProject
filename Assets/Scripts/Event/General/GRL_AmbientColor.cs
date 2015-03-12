79b0cae9525adbd3b1319be30542d73082f0676b
Palaui <sucdepressec@gmail.com>
2015-03-12 16:18:52 +0100

    5th instance events

---
+++ Assets/Scripts/Event/General/GRL_AmbientColor.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GRL_AmbientColor : MonoBehaviour 
+{
+    public Color ambient;
+
+    private float timeCounter = 0;
+    private bool active;
+
+
+	void Update ()
+    {
+        if (active)
+        {
+            EventsLib.UpdateRenderambient();
+            EventsLib.UpdateDoorOpenDoubleSlider();
+
+            timeCounter -= Time.deltaTime;
+            if (timeCounter < 0)
+                active = false;
+        }
+	}
+
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            EventsLib.SetRenderambient(ambient);
+
+            timeCounter = 10;
+            active = true;
+        }
+    }
+}
