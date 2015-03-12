516beec78c2c39ad000c1400b6e0af89cecf1dca
AkuseruSan <akuseruakai@gmail.com>
2015-03-12 20:37:52 +0100

    Fire Gem Added and more Particles

---
+++ Assets/Resources/Props/Geometry/Fire Gem/FireGemAnim.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireGemAnim : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(0, 0.7f, 0), Space.World);
+	}
+}
